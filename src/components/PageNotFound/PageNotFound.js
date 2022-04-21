7040b56998d1bbf96609dc6fc841f8a5e9e4fc7b
manchivarusanthosh <manchivarusanthoshkumar@gmail.com>
2022-04-21 14:12:25 +0530

    all changes done

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,8 +1,14 @@
 import React from "react";
+import pageNotFoundImg from "../../images/pnf.jpg";
+import "./PageNotFound.scss";
 
 const PageNotFound = () => {
   console.log("page not found");
-  return <div>PageNotFound</div>;
+  return (
+    <div className="page-not-found">
+      <img src={pageNotFoundImg} alt="PageNotFound" />
+    </div>
+  );
 };
 
 export default PageNotFound;
